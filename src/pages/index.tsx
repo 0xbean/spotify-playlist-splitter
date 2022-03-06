c39728d915cc9494fdca7b72917652ce89f65a16
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2022-03-06 16:12:17 -0500

    [feat] Adding in video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { Text, List, ListIcon, ListItem, Flex } from '@chakra-ui/react';
 
 const Index = () => (
   <Flex>
+    <video autoPlay muted loop id="main-bg">
+      <source src="/videos/main-bg.mp4" type="video/mp4" />
+    </video>
     <Text>hello</Text>
   </Flex>
 );
