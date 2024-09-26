a6e5625506e6db7608d738d0e0bd73e377a87f79
kimjeff49 <jeffkkim612@gmail.com>
2024-09-26 10:25:20 -0400

    adding in some changes to drag drop

--- src/components/DragDropBox.vue
+++ src/components/DragDropBox.vue
@@ -1,5 +1,56 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTemplateRef, onMounted, ref } from 'vue'
+
+const box = useTemplateRef<HTMLDivElement>('box')
+
+onMounted(() => {
+  if (box.value) {
+    box.value.onmousedown = (e) => {
+      box.value.draggable = true
+    }
+
+    box.value.onmouseup = () => {
+      box.value.draggable = false
+    }
+
+    box.value.ondragstart = (event: DragEvent) => {
+      const ghostElement = document.createElement('div')
+
+      event.dataTransfer.setDragImage(
+        ghostElement,
+        ghostElement.offsetWidth / 2,
+        ghostElement.offsetHeight / 2
+      )
+    }
+
+    // box.value.addEventListener('mousedown', (e) => {
+    //   x2.value = e.clientX
+    //   y2.value = e.clientY
+    // })
+
+    // box.value.addEventListener('mousemove', (e) => {
+    //   x1.value = x2.value - e.clientX
+    //   y1.value = y2.value - e.clientY
+    //   x2.value = e.clientX
+    //   y2.value = e.clientY
+
+    //   box.value.style.top = box.value.offsetTop - y1.value + 'px'
+    //   box.value.style.left = box.value.offsetLeft - x1.value + 'px'
+    // })
+
+    // box.value.addEventListener('mouseup', () => {})
+  }
+})
+</script>
 
 <template>
-  <div class="border-gray-600 rounded-md w-full h-20 border-2"></div>
+  <div class="relative">
+    <div
+      class="border-gray-400 rounded-md w-full h-20 border-2 hover:cursor-grab hover:border-gray-600 active:cursor-grabbing transition-all"
+      ref="box"
+    ></div>
+    <div
+      class="border-gray-400 rounded-md absolute top-0 -z-10 w-full h-20 border-2 border-dotted hover:border-gray-600"
+    ></div>
+  </div>
 </template>
--- src/components/DragDropContainer.vue
+++ src/components/DragDropContainer.vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
 import { ref, useTemplateRef, onMounted } from 'vue'
+import DragDropBox from './DragDropBox.vue'
 
 const boxes = ref(0)
 const container = useTemplateRef<HTMLDivElement>('container')
 
 onMounted(() => {
   if (container.value) {
-    container.value.addEventListener(
-      'mouseup',
-      () => {
-        console.log('up!')
-      },
-      false
-    )
+    // container.value.addEventListener(
+    //   'mouseup',
+    //   () => {
+    //     console.log('up!')
+    //   },
+    //   false
+    // )
   }
 })
 </script>
 
 <template>
-  <div class="border-gray-800 border-2 w-1/6 h-1/2 rounded-md p-3" ref="container">
-    <div class="w-full text-center border-2 border-gray-400 rounded-md p-1 cursor-pointer">
+  <div
+    class="border-gray-800 border-2 w-1/6 h-1/2 rounded-md p-3 overflow-y-scroll space-y-3 scrollbar"
+    ref="container"
+  >
+    <div
+      class="w-full text-center border-2 border-gray-400 rounded-md p-1 cursor-pointer select-none"
+      v-on:click="boxes++"
+    >
       + item
     </div>
+    <template v-for="box of boxes" v-bind:key="box">
+      <DragDropBox />
+    </template>
   </div>
 </template>
